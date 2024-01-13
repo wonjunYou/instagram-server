e40c2c89969e8f222b4bc136ddfa8779086aeafe
wonjunYou <youwjune@gmail.com>
2024-01-13 20:31:59 +0900

    build : swagger 초기 세팅

--- src/main/java/numble/instagram/InstagramApplication.java
+++ src/main/java/numble/instagram/InstagramApplication.java
@@ -2,8 +2,9 @@ package numble.instagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class InstagramApplication {
 
     public static void main(String[] args) {
