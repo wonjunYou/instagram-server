2564029862a3e0b73d4a9a64b2edceedadc31931
wonjunYou <youwjune@gmail.com>
2024-01-17 21:18:02 +0900

    feat : Member 엔티티 및 관련 값 타입 작성

---
+++ src/main/java/domain/member/Gender.java
@@ -0,0 +1,13 @@
+package domain.member;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Gender {
+    MAN("남성"),
+    WOMAN("여성");
+
+    private final String value;
+}
---
+++ src/main/java/domain/member/Role.java
@@ -0,0 +1,19 @@
+package domain.member;
+
+import java.util.Arrays;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    USER("ROLE_USER");
+
+    private final String role;
+
+    private List<String> getRoles() {
+        return Arrays.asList(role.split(","));
+    }
+}
