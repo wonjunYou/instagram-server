8443f2eb0a2dca14bd05d5453f7c878ea55ab17a
wonjunYou <youwjune@gmail.com>
2024-01-27 11:34:12 +0900

    refactor : timebaseEntity를 LocalDateTime을 Timestamp로 변경

--- src/main/java/numble/instagram/common/entity/TimeBaseEntity.java
+++ src/main/java/numble/instagram/common/entity/TimeBaseEntity.java
@@ -3,23 +3,23 @@ package numble.instagram.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
+@NoArgsConstructor
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class TimeBaseEntity {
 
-    @CreatedDate
-    @Column(columnDefinition = "TIMESTAMP", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Timestamp createdAt;
 
-    @LastModifiedDate
-    @Column(columnDefinition = "TIMESTAMP")
-    private LocalDateTime updatedAt;
-
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 }
